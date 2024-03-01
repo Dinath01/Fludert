a257ba470edb42e9b28e7d0563cca84ff9ca5345
Ravindu Dahanayake <147282405+RavinduDahanayake2002@users.noreply.github.com>
2024-03-01 19:06:31 +0530

    About Us page
    Partially Completed

--- website/src/pages/About.js
+++ website/src/pages/About.js
@@ -1,17 +1,140 @@
 import React from "react";
 import Nav from "../components/Nav.js";
+import "../components/style/about.css";
+import Footer from "../components/Footer.js";
 
 export default function About() {
   return (
     <div>
       <Nav />
-      <div class="h-56 grid grid-cols-3 gap-10 content-start ... ">
-        <div class="bg-sky-500/100 ... mt-40 ...  ml-10...">01</div>
-        <div class="bg-sky-500/100 ...">02</div>
-        <div class="bg-sky-500/100 ...">03</div>
-        <div>04</div>
-        <div>05</div>
-      </div>
+      <section>
+        <div class="box">
+          <div class="students_details">
+            <div class="arrow-left"></div>
+            <div class="arrow-right"></div>
+            <div class="student-details-content" id="student-details">
+              <a href="yeshan.html" class="card">
+                <div class="card-top">
+                  <div class="card-image">
+                    {/* <img src="/images/quiz/6.jpeg" alt=""> */}
+                  </div>
+                </div>
+                <div class="card-bottom">
+                  <div class="card-bottom-content">
+                    <h3>Dinath</h3>
+                    <p>Software Engineering</p>
+                    <p>IIT ID - 0000000</p>
+                    <p>UoW ID - 0000000</p>
+                    <p>Page Role - Student 3</p>
+                  </div>
+                </div>
+
+                <div class="left-page-down"></div>
+                <div class="right-page-down"></div>
+              </a>
+
+              <a href="yeshan.html" class="card">
+                <div class="card-top">
+                  <div class="card-image">
+                    {/* <img src="/images/quiz/9.jpeg" alt=""> */}
+                  </div>
+                </div>
+                <div class="card-bottom">
+                  <div class="card-bottom-content">
+                    <h3>Ravindu</h3>
+                    <p>Software Engineering</p>
+                    <p>IIT ID - 0000000</p>
+                    <p>UoW ID - 0000000</p>
+                    <p>Page Role - Student 3</p>
+                  </div>
+                </div>
+
+                <div class="left-page-down"></div>
+                <div class="right-page-down"></div>
+              </a>
+
+              <a href="yeshan.html" class="card">
+                <div class="card-top">
+                  <div class="card-image">
+                    {/* <img src="/images/quiz/6.jpeg" alt=""> */}
+                  </div>
+                </div>
+                <div class="card-bottom">
+                  <div class="card-bottom-content">
+                    <h3>Trevin</h3>
+                    <p>Software Engineering</p>
+                    <p>IIT ID - 0000000</p>
+                    <p>UoW ID - 0000000</p>
+                    <p>Page Role - Student 3</p>
+                  </div>
+                </div>
+
+                <div class="left-page-down"></div>
+                <div class="right-page-down"></div>
+              </a>
+
+              <a href="yeshan.html" class="card">
+                <div class="card-top">
+                  <div class="card-image">
+                    {/* <img src="/images/quiz/9.jpeg" alt=""> */}
+                  </div>
+                </div>
+                <div class="card-bottom">
+                  <div class="card-bottom-content">
+                    <h3>Shaleen</h3>
+                    <p>Software Engineering</p>
+                    <p>IIT ID - 0000000</p>
+                    <p>UoW ID - 0000000</p>
+                    <p>Page Role - Student 3</p>
+                  </div>
+                </div>
+
+                <div class="left-page-down"></div>
+                <div class="right-page-down"></div>
+              </a>
+              <a href="yeshan.html" class="card">
+                <div class="card-top">
+                  <div class="card-image">
+                    {/* <img src="/images/quiz/9.jpeg" alt=""> */}
+                  </div>
+                </div>
+                <div class="card-bottom">
+                  <div class="card-bottom-content">
+                    <h3>Kasun</h3>
+                    <p>Software Engineering</p>
+                    <p>IIT ID - 0000000</p>
+                    <p>UoW ID - 0000000</p>
+                    <p>Page Role - Student 3</p>
+                  </div>
+                </div>
+
+                <div class="left-page-down"></div>
+                <div class="right-page-down"></div>
+              </a>
+              <a href="yeshan.html" class="card">
+                <div class="card-top">
+                  <div class="card-image">
+                    {/* <img src="/images/quiz/9.jpeg" alt=""> */}
+                  </div>
+                </div>
+                <div class="card-bottom">
+                  <div class="card-bottom-content">
+                    <h3>Tharani</h3>
+                    <p>Software Engineering</p>
+                    <p>IIT ID - 0000000</p>
+                    <p>UoW ID - 0000000</p>
+                    <p>Page Role - Student 3</p>
+                  </div>
+                </div>
+
+                <div class="left-page-down"></div>
+                <div class="right-page-down"></div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </div>
   );
 }
