3de86fe450a4fdbee7c99c99c247d21be419413d
Ravindu Dahanayake <147282405+RavinduDahanayake2002@users.noreply.github.com>
2024-03-14 20:21:00 +0530

    About Us page
    Update part of about us page

--- website/src/pages/About.js
+++ website/src/pages/About.js
@@ -1,17 +1,105 @@
 import React from "react";
 import Nav from "../components/Nav.js";
+import "../components/style/about.css";
 
 export default function About() {
   return (
     <div>
       <Nav />
-      <div class="h-56 grid grid-cols-3 gap-10 content-start ... ">
-        <div class="bg-sky-500/100 ... mt-40 ...  ml-10...">01</div>
-        <div class="bg-sky-500/100 ...">02</div>
-        <div class="bg-sky-500/100 ...">03</div>
-        <div>04</div>
-        <div>05</div>
-      </div>
+      <section>
+                <div class="box">
+                    <div class="students_details">
+                        <div class="arrow-left">
+                            <i class="ri-arrow-left-circle-fill" id="left-arrow"></i>
+                        </div>
+                        <div class="arrow-right">
+                            <i class="ri-arrow-right-circle-fill" id="right-arrow"></i>
+                        </div>
+                        <div class="student-details-content" id="student-details">
+                            <a href="yeshan.html" class="card">
+                                <div class="card-top">
+                                    <div class="card-image">
+                                        {/* <img src="/images/quiz/6.jpeg" alt=""> */}
+                                    </div>
+                                </div>
+                                <div class="card-bottom">
+                                    <div class="card-bottom-content">
+                                        <h3>Yeshan</h3>
+                                        <p>Software Engineering</p>
+                                        <p>IIT ID - 0000000</p>
+                                        <p>UoW ID - 0000000</p>
+                                        <p>Page Role - Student 3</p>
+                                    </div>
+                                </div>
+
+                                <div class="left-page-down"></div>
+                                <div class="right-page-down"></div>
+                            </a>
+
+                            <a href="yeshan.html" class="card">
+                                <div class="card-top">
+                                    <div class="card-image">
+                                        {/* <img src="/images/quiz/9.jpeg" alt=""> */}
+                                    </div>
+                                </div>
+                                <div class="card-bottom">
+                                    <div class="card-bottom-content">
+                                        <h3>Yeshan</h3>
+                                        <p>Software Engineering</p>
+                                        <p>IIT ID - 0000000</p>
+                                        <p>UoW ID - 0000000</p>
+                                        <p>Page Role - Student 3</p>
+                                    </div>
+                                </div>
+
+                                <div class="left-page-down"></div>
+                                <div class="right-page-down"></div>
+                            </a>
+
+                            <a href="yeshan.html" class="card">
+                                <div class="card-top">
+                                    <div class="card-image">
+                                        {/* <img src="/images/quiz/6.jpeg" alt=""> */}
+                                    </div>
+                                </div>
+                                <div class="card-bottom">
+                                    <div class="card-bottom-content">
+                                        <h3>Yeshan</h3>
+                                        <p>Software Engineering</p>
+                                        <p>IIT ID - 0000000</p>
+                                        <p>UoW ID - 0000000</p>
+                                        <p>Page Role - Student 3</p>
+                                    </div>
+                                </div>
+
+                                <div class="left-page-down"></div>
+                                <div class="right-page-down"></div>
+                            </a>
+
+                            <a href="yeshan.html" class="card">
+                                <div class="card-top">
+                                    <div class="card-image">
+                                        {/* <img src="/images/quiz/9.jpeg" alt=""> */}
+                                    </div>
+                                </div>
+                                <div class="card-bottom">
+                                    <div class="card-bottom-content">
+                                        <h3>Yeshan</h3>
+                                        <p>Software Engineering</p>
+                                        <p>IIT ID - 0000000</p>
+                                        <p>UoW ID - 0000000</p>
+                                        <p>Page Role - Student 3</p>
+                                    </div>
+                                </div>
+
+                                <div class="left-page-down"></div>
+                                <div class="right-page-down"></div>
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
     </div>
   );
 }
