15e8f052bef581b23b1364fe61c07e0367538923
shaleen-vihanga <116054581+shaleen-vihanga@users.noreply.github.com>
2024-03-14 20:52:34 +0530

    profile screen structure created

---
+++ lib/pages/profile_screen.dart
@@ -0,0 +1,23 @@
+import 'package:fludert/assets/navbar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Navbar(
+        body: Container(
+          color: Colors.amber,
+        ),
+      ),
+      // bottomNavigationBar: Navbar(),
+    );
+  }
+}
