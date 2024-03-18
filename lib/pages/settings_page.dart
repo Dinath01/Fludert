5b985ddcd1261c6318badb7bf2db3ddb0c271674
shaleen-vihanga <116054581+shaleen-vihanga@users.noreply.github.com>
2024-03-18 21:34:50 +0530

    create setting page structure

---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      body: Center(
+        child: Text(
+        'Setting',),
+      ),
+    );
+  }
