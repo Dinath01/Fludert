03fea4f57d4900044a8c64c7b1df347f7e6fdac0
shaleen-vihanga <116054581+shaleen-vihanga@users.noreply.github.com>
2024-03-18 21:35:43 +0530

    alert page dummy created

---
+++ lib/pages/alert_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AlertPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Alerts',
+          ),)
+    );
+  }
+}
