8558cd35ec1d17a7f93cd6cc53482f424a9a6735
shaleen-vihanga <116054581+shaleen-vihanga@users.noreply.github.com>
2024-03-19 03:28:11 +0530

    worked on profile screen

--- lib/pages/profile_screen.dart
+++ lib/pages/profile_screen.dart
@@ -1,7 +1,8 @@
-import 'package:fludert/assets/navbar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
+
+import 'package:flutter/widgets.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -12,12 +13,51 @@ class ProfileScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Profile'),
       ),
-      // body: Navbar(
-      body: Container(
-        color: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            // const SizedBox(height: 40),
+            CircleAvatar(
+              radius: 70,
+              backgroundImage: AssetImage('assets/images/profile.png'),
+            ),
+            const SizedBox(height: 20),
+            itemProfile('Name', 'John Dew', CupertinoIcons.person),
+            const SizedBox(height: 10),
+            itemProfile('Phone', '0772211005', CupertinoIcons.phone),
+            const SizedBox(height: 10),
+            itemProfile('Address', 'Gampola Kandy', CupertinoIcons.location),
+            const SizedBox(height: 10),
+            itemProfile('Email', 'joindew@hotmail.com', CupertinoIcons.mail),
+
+          ],
+        ),
       ),
-      // ),
-      // bottomNavigationBar: Navbar(),
+    );
+  }
+
+  itemProfile(String title, String subtitle, IconData iconData){
+    return Container(
+      decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0,5),
+                    color: Colors.purple.withOpacity(.2),
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                  )
+                ]
+              ),
+              child: ListTile(
+                title: Text(title),
+                subtitle: Text(subtitle),
+                leading: Icon(iconData),
+                trailing: Icon(Icons.arrow_forward, color: Colors.grey,),
+                tileColor: Colors.white,
+              ),
     );
   }
 }
