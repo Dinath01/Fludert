098d6dc0023348ed380e2f7d844e25ee46034d5c
Dinath01 <dinithnethmina101@gmail.com>
2024-03-24 19:36:12 +0530

    Resetted the home page to implement with the API call

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,6 @@
-import 'dart:math';
-import 'dart:ui';
-import 'package:fludert/assets/navbar.dart';
-import 'package:fludert/assets/weather_map.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:intl/intl.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -34,244 +29,147 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String location = 'Colombo';
-  List<String> cities = ['Colombo', 'Matara', 'Jaffna'];
-  List<dynamic> consolidatedWeatherList = List.generate(
-    7,
-    (index) => {
-      'id': Random().nextInt(1000),
-      'weather_state_name': 'Sunny',
-      'wind_speed': Random().nextInt(10) + 1,
-      'applicable_date': DateTime.now().add(Duration(days: index)).toString(),
-      'the_temp': Random().nextInt(30) + 15,
-    },
-  );
+  late String location;
+  final String apiKey = '552ea31084a4a82bf8ce26477f4dc33c';
+  Map<String, dynamic> weatherData = {};
 
-  void _changeLocation(String? newLocation) {
-    if (newLocation != null) {
+  final TextEditingController _locationController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    location = 'Colombo'; // Default location
+    fetchWeather();
+  }
+
+  Future<void> fetchWeather() async {
+    try {
+      final data = await fetchWeatherData(location, apiKey);
       setState(() {
-        location = newLocation;
-        consolidatedWeatherList = List.generate(
-          7,
-          (index) => {
-            'id': Random().nextInt(1000),
-            'weather_state_name': 'Sunny',
-            'wind_speed': Random().nextInt(10) + 1,
-            'applicable_date':
-                DateTime.now().add(Duration(days: index)).toString(),
-            'the_temp': Random().nextInt(30) + 15,
-          },
-        );
+        weatherData = data;
       });
+    } catch (e) {
+      print('Error fetching weather data: $e');
     }
   }
 
-  String floodSeverity = 'Severe'; 
+  Future<Map<String, dynamic>> fetchWeatherData(String city, String apiKey) async {
+    final response = await http.get(Uri.parse('http://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey'));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(location),
-        actions: [
-          DropdownButton<String>(
-            value: location,
-            onChanged: _changeLocation,
-            items: cities.map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-          ),
-        ],
       ),
-      // body : Navbar(
-      body: Container(
-        color: Colors.white,
+      body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  Text(
-                    location,
-                    style: TextStyle(fontSize: 30),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _locationController,
+                decoration: InputDecoration(
+                  labelText: 'Enter Location',
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        location = _locationController.text;
+                        fetchWeather();
+                      });
+                    },
+                    icon: Icon(Icons.search),
                   ),
-                  Text(
-                    DateTime.now().toString().substring(0, 10),
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(height: 20),
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.blue[200],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              '25°',
-                              style: TextStyle(fontSize: 30),
-                            ),
-                            Text(
-                              'Feels like 22°',
-                              style: TextStyle(fontSize: 14),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Image.asset(
-                              'assets/images/clear.png',
-                              width: 100,
-                              height: 100,
-                            ),
-                            Text(
-                              'Sunny',
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Text(
-                              '10km/h',
-                              style: TextStyle(fontSize: 16),
-                            ),
-                            Image.asset(
-                              'assets/images/lightcloud.png',
-                              width: 50,
-                              height: 50,
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Text(
-                              '60%',
-                              style: TextStyle(fontSize: 16),
-                            ),
-                            Image.asset(
-                              'assets/images/humidity.png',
-                              width: 50,
-                              height: 50,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  FloodSeverityCard(severityLevel: floodSeverity),
-                  
-
-                ],
+                ),
               ),
             ),
-            
-            //WeatherMapWidget()
+            if (weatherData.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: WeatherCard(
+                  temperature: weatherData['main']['temp'],
+                  weatherCondition: weatherData['weather'][0]['main'],
+                  windSpeed: weatherData['wind']['speed'],
+                ),
+              ),
           ],
         ),
       ),
-      // ),
-      //bottomNavigationBar: Navbar(),
     );
   }
 }
 
-class FloodSeverityCard extends StatelessWidget {
-  final String severityLevel;
+class WeatherCard extends StatelessWidget {
+  final double temperature;
+  final String weatherCondition;
+  final double windSpeed;
 
-  const FloodSeverityCard({Key? key, required this.severityLevel})
-    : super(key: key); 
+  const WeatherCard({
+    required this.temperature,
+    required this.weatherCondition,
+    required this.windSpeed,
+    Key? key,
+  }) : super(key: key);
 
-    Color _getColorForSeverity(String severityLevel) {
-    switch (severityLevel.toLowerCase()) {
-      case 'severe':
-        return Colors.red;
-      case 'mid':
-        return Colors.orange;
-      case 'safe':
-        return Colors.green;
+  String getIconForWeatherCondition(String condition) {
+    switch (condition.toLowerCase()) {
+      case 'clear':
+        return 'assets/images/clear.png';
+      case 'clouds':
+        return 'assets/images/lightcloud.png';
+      case 'rain':
+        return 'assets/images/heavyrain.png';
+      case 'snow':
+        return 'assets/images/snow.png';
       default:
-        return Colors.grey;
+        return 'assets/images/max-temp.png';
     }
-  }  
+  }
 
   @override
   Widget build(BuildContext context) {
-    Color backgroundColor = _getColorForSeverity(severityLevel);
-        String assetName = '';
-    
-    switch (severityLevel.toLowerCase()) {
-      case 'severe':
-        assetName = 'assets/images/Severe.png';
-        break;
-      case 'mid':
-        assetName = 'assets/images/Mid.png';
-        break;
-      case 'safe':
-        assetName = 'assets/images/Light.png';
-        break;
-      default:
-        assetName = '';
-        break;
-    }
-
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.white70.withOpacity(1),
-            spreadRadius: 3,
-            blurRadius: 5,
-            offset: Offset(0,3),
-          )
-        ],
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            backgroundColor.withOpacity(0.8),
-            backgroundColor.withOpacity(1),
-          ]
-        )
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [Text(
-          "Flood Severity",
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,),
-          textAlign: TextAlign.center,
-        ),
-          SizedBox(height: 10),
-          Text(
-            'Severity Level: $severityLevel',
-            style: TextStyle(fontSize: 16),
-            textAlign: TextAlign.center,
-          ),
-          // Add relevant image based on severity level
-          if (assetName.isNotEmpty)
-            Center(
-              child: Image.asset(
-                assetName,
-                width: 60,
-                height: 60,
-              ),
+    return Card(
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Temperature: $temperature °C',
+              style: TextStyle(fontSize: 18),
             ),
-        ],
+            SizedBox(height: 8),
+            Row(
+              children: [
+                Image.asset(
+                  getIconForWeatherCondition(weatherCondition),
+                  width: 32,
+                  height: 32,
+                ),
+                SizedBox(width: 8),
+                Text(
+                  'Weather: $weatherCondition',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Wind Speed: $windSpeed m/s',
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
